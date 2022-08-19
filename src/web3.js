13aa29b5f19e4f3813638be0d4e1bf0c1453a424
August G <august.gran@gmail.com>
2022-08-19 17:38:29 +0200

    remove comment

--- src/web3.js
+++ src/web3.js
@@ -4,7 +4,7 @@ const Web3 = require('web3');
 require('dotenv').config();
 
 const provider = new HDWalletProvider(
-  'grant bag anchor cook mule tourist surprise father label safe sustain cigar', // remember to change this to your own phrase!
+  'grant bag anchor cook mule tourist surprise father label safe sustain cigar',
   process.env.MAINNET_INFURA_API_KEY //make a .env inside /src and insert MAINNET_INFURA_API_KEY=https://mainnet.infura.io/v3/YOUR_KEY_HERE
 );
 const web3 = new Web3(provider);
